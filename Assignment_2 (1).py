96504e4d64966abfe8a87b7efec111ccadff8189
super99666 <66785888+super99666@users.noreply.github.com>
2020-07-20 14:53:05 +0530

    Add files via upload

---
+++ Assignment_2 (1).py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+num=int(input("Enter a number:"))
+for i in range(1,num+1):
+    for j in range(1,i+1):
+        print("*",end="")
+    print()
+for a in range(1,num+1):
+    for a in range(1,num+1-a):
+        print("*",end="")
+    print()   
+
+
+# In[24]:
+
+
+def reverse(string): 
+    string = string[::-1] 
+    return string 
+  
+s = "ineuron"
+  
+print (s) 
+  
+print (reverse(s))
+
+
+# In[17]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
